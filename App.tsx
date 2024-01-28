7b3df4eb2921698a671dc05c2a71d95b2fde027f
KUROSAKI <xxxbijumrzaxxx@gmail.com>
2024-01-28 11:42:50 +0700

    test_gameContainer

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  Image,
   View,
 } from 'react-native';
 
@@ -21,16 +21,19 @@ const App = () =>  {
         <ScrollView>
           <View style={styles.mainContainer}>
             <View style={styles.gameContainer}>
-              <Text>Hello</Text>
+              <View style={styles.gameImageContainer} >
+                <Image source={require("./Assets/Posters/poster_Resident_Evil.jpeg")} style={styles.gameImage}/>
+              </View>
+              <View style={styles.gameNameContainer}>
+                <Text style={styles.gameNameText}> Resident Evil 4 </Text>
+              </View>
+              <Text style={styles.priceText}> от 1000 ₽</Text>
             </View>
             <View style={styles.gameContainer}>
-              <Text>Hello</Text>
             </View>
             <View style={styles.gameContainer}>
-              <Text>Hello</Text>
             </View>
             <View style={styles.gameContainer}>
-              <Text>Hello</Text>
             </View>
           </View>
         </ScrollView>
@@ -65,18 +68,41 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
   },
   gameContainer: {
-    height: 175,
-    width: 115,
+    height: 270,
+    width: 160,
     borderRadius: 15,
-    borderColor: 'red',
-    borderWidth: 1,
-    backgroundColor: 'green',
+    backgroundColor: '#ebebeb',
     marginRight: 10,
     marginBottom: 10,
+    marginLeft: 10,
     marginTop: 10,
+    overflow: 'hidden',
+  },
+  gameImageContainer: {
+    height: 200,
+    width: '100%',
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  gameNameContainer: {
+    height: 35,
+    width: '100%',
+    justifyContent: 'center',
   },
   gameImage: {
-
+    height: '100%',
+    width: '100%',
+    resizeMode: "cover",
+  },
+  gameNameText: {
+    fontSize: 14,
+    color: 'black',
+    textAlign: 'center',
+  },
+  priceText: {
+    fontSize: 14,
+    color: 'black',
+    textAlign: 'center',
   },
 });
 
